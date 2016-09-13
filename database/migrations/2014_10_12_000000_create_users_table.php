27e928b8d7f247dca18d34f1ec3494827f84f351
jhunexjun <jdream_catcher@yahoo.com>
2016-09-14 04:28:16 +0800

    Creating and modifying Users table.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('test', 60)->unique();
         });
     }
 
