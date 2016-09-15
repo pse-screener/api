aa7bc4d5a87baf4981abe208f1a994905ec933bc
jhunexjun <jdream_catcher@yahoo.com>
2016-09-15 12:49:33 +0800

    Started composer require laravel/passport, ended before php artisan vendor:publish --tag=passport-components

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // Passport
+        Laravel\Passport\PassportServiceProvider::class,
     ],
 
     /*
